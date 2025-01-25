0b67caf0d82491a760ac2e9be6be448048fb28d8
DanBeverley <hoap43431@gmail.com>
2025-01-25 10:30:53 +0700

    commit

--- training.py
+++ training.py
@@ -1,4 +1,5 @@
 import os
+import json
 import random
 
 import model
@@ -11,7 +12,7 @@ import torch
 from tqdm import tqdm
 from torch.utils.data import DataLoader
 
-def accuracy_fn(predictions, targets):
+def accuracy_fn(predictions, targets, pad_token):
     """Compute accuracy of predictions"""
     _, pred_ids = torch.max(predictions, dim=-1)
     correct = (pred_ids == targets).float()
@@ -19,57 +20,53 @@ def accuracy_fn(predictions, targets):
     return (correct*mask).sum().item()/mask.sum().item()
 
 if __name__ == "__main__":
-    num_classes = 429
-    d_model = 512
-    num_layers = 6
-    num_heads = 8
-    dff = 2048
-    dropout_rate = 0.1
-    max_seq_len = 512
-    pad_token = 0
-    learning_rate = 1e-4
-    smoothing = 0.1
-    num_epochs = 4
-    batch_size = 32
-    clip_value = 1.0
-    generation_length = 100 # Length of generated sequence for evaluation
-    checkpoint_dir = "./checkpoints"
+    with open("config.json", "r") as f:
+        config = json.load(f)
+
+    model_params = config["model_params"]
+    training_params = config["training_params"]
+    scheduler_params = config["scheduler"]
+    data_paths = config["data_paths"]
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    model_ = model.MusicTransformer(num_classes = num_classes,
-                                    d_model = d_model,
-                                    num_layers = num_layers,
-                                    num_heads = num_heads,
-                                    dff = dff,
-                                    dropout_rate = dropout_rate,
-                                    max_seq_len = max_seq_len,
-                                    pad_token = pad_token).to(device)
+    model_ = model.MusicTransformer(num_classes = model_params["num_classes"],
+                                    d_model = model_params["d_model"],
+                                    num_layers = model_params["num_layers"],
+                                    num_heads = model_params["num_heads"],
+                                    dff = model_params["dff"],
+                                    dropout_rate = model_params["dropout_rate"],
+                                    max_seq_len = model_params["max_seq_len"],
+                                    pad_token = model_params["pad_token"]).to(device)
 
-    criterion = get_loss_function("cross_entropy", model_.embedding.num_embeddings, smoothing=0.1,
-                                  pad_token = model_.pad_token)
-    optimizer = get_optimizer("adamW", model_, learning_rate=1e-4)
+    criterion = get_loss_function(config["loss_function"], model_.embedding.num_embeddings,
+                                  smoothing=training_params["smoothing"],
+                                  pad_token = model_params["pad_token"])
+    optimizer = get_optimizer(config["optimizer"], model_,
+                              learning_rate=training_params["learning_rate"])
 
     # Learning rate scheduler
     # scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
-    scheduler = get_scheduler("step", optimizer, step_size = 10 , gamma=.1)
+    scheduler = get_scheduler(scheduler_params["type"], optimizer,
+                              step_size = scheduler_params["step_size"] ,
+                              gamma = scheduler_params["gamma"])
 
     # Dataloader
-    train_loader = DataLoader(subset_train, batch_size=batch_size, shuffle=True,
-                              collate_fn=lambda x:collate_fn(x, pad_token))
-    val_loader   = DataLoader(subset_valid, batch_size=batch_size, shuffle=False,
-                              collate_fn=lambda x:collate_fn(x, pad_token))
+    train_loader = DataLoader(subset_train, batch_size=training_params["batch_size"],
+                              shuffle=True, collate_fn=lambda x:collate_fn(x, model_params["pad_token"]))
+    val_loader   = DataLoader(subset_valid, batch_size=training_params["batch_size"],
+                              shuffle=False, collate_fn=lambda x:collate_fn(x, model_params["pad_token"]))
 
     # Metrics
     metrics = {"accuracy":accuracy_fn}
 
     # Training Loop
-    os.makedirs(checkpoint_dir, exist_ok=True)
+    os.makedirs(data_paths["checkpoint_dir"], exist_ok=True)
 
-    for epoch in range(num_epochs):
+    for epoch in range(training_params["num_epochs"]):
         model_.train()
         epoch_loss = 0
-        for inputs in tqdm(train_loader, desc=f"Epoch {epoch+1}/{num_epochs}"):
+        for inputs in tqdm(train_loader, desc=f"Epoch {epoch+1}/{training_params["num_epochs"]}"):
             inputs_ids = inputs["input_ids"].to(device)
             targets    = inputs["labels"].to(device)
 
@@ -85,7 +82,8 @@ if __name__ == "__main__":
             # Backward pass and optimization
             optimizer.zero_grad()
             loss.backward()
-            torch.nn.utils.clip_grad_norm_(model_.parameters(), max_norm=clip_value)
+            torch.nn.utils.clip_grad_norm_(model_.parameters(),
+                                           max_norm=training_params["clip_value"])
             optimizer.step()
 
             epoch_loss += loss.item()
@@ -105,19 +103,19 @@ if __name__ == "__main__":
         if epoch%1==0: # Generate for every epoch
             model_.eval()
             with torch.no_grad():
-                start_sequence = torch.tensor([random.randint(1, num_classes-1)],
+                start_sequence = torch.tensor([random.randint(1, model_params["num_classes"]-1)],
                                               device=device).unsqueeze(0) # Random start token
                 generated_sequence = start_sequence.clone()
-                for _ in range(generation_length):
+                for _ in range(training_params["generation_length"]):
                     output = model_(generated_sequence)
                     next_token = output[:, -1, :].argmax(dim=-1, keepdim=True)
                     generated_sequence = torch.cat([generated_sequence, next_token], dim=1)
-                    if next_token.item() == pad_token: # Stop on pad token
+                    if next_token.item() == model_params["pad_token"]: # Stop on pad token
                         break
                 print(f"Generated Sequence : {generated_sequence.tolist()}")
 
         # Save checkpoint
-        torch.save(model_.state_dict(),os.path.join(checkpoint_dir,
+        torch.save(model_.state_dict(),os.path.join(data_paths["checkpoint_dir"],
                                                     f"model_epoch{epoch + 1}.pth"))
         scheduler.step()
 
